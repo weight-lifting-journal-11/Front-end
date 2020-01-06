84d9a42807940e2b5cee0d717bc0a8b0f259987a
CAM603 <cameronhawley@comcast.net>
2020-01-06 16:41:17 -0500

    form in journal list adds hardcoded input

--- weight-lifting-journal-front-end/src/components/Common/JournalList.js
+++ weight-lifting-journal-front-end/src/components/Common/JournalList.js
@@ -10,6 +10,49 @@ const JournalList = () => {
     // Set state for loading
     const [loading, setLoading] = useState(true)
 
+    // Get todays date in mm/dd/yyyy format
+    let today = new Date();
+    let day = String(today.getDate()).padStart(2, '0');
+    let month = String(today.getMonth() + 1).padStart(2, '0');
+    let year = today.getFullYear();
+    today = `${month}/${day}/${year}`;
+
+    // State for form
+    const [journal, setJournal] = useState({
+        id: '',
+        userId: '',
+        date: today,
+        region: ''
+    })
+    // Handle input change
+    const handleChange = event => {
+        setJournal({
+            ...journal,
+            [event.target.name]: event.target.value
+        })
+        console.log(journal)
+    }
+    // Handle form submit
+    const handleSubmit = event => {
+        event.preventDefault();
+        addNewJournal(journal)
+        setJournal({
+            id: '',
+            userId: '',
+            date: today,
+            region: ''
+        })
+    }
+    // Add new journal to state
+    const addNewJournal = journal => {
+        const newJournal = {
+            id: journal.id,
+            userId: journal.userId,
+            date: today,
+            region: journal.region
+        }
+        setJournals([...journals, newJournal]);
+    }
     // axios call
     useEffect(() => {
         setLoading(true)
@@ -28,8 +71,18 @@ const JournalList = () => {
     return (
         
         <div>
-            // CreateJournal form to create a new workout
             <h1>Journal list</h1>
+            <h2>Add Journal Entry</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input
+                onChange={handleChange}
+                name="region"
+                type="text"
+                value={journal.region}
+                />
+                <button type="submit">Submit</button>
+            </form>
             {journals.map(journal => (
                 <JournalCard 
                 key={journal.id} 
