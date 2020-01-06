a9db52f8b12fd2a5cd4d8ac60721518aece853f9
CAM603 <cameronhawley@comcast.net>
2020-01-06 14:25:30 -0500

    added axios call in journal entry

--- weight-lifting-journal-front-end/src/components/Common/JournalEntry.js
+++ weight-lifting-journal-front-end/src/components/Common/JournalEntry.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const JournalEntry = () => {
+    const [loading, setLoading] = useState(true);
+    const [exercises, setExercises] = useState([]);
+
+    const { id } = useParams();
+    
+    useEffect(() => {
+        setLoading(true);
+
+        axios
+        .get()
+        .then(response => {
+            console.log(response);
+
+        })
+        .catch(error => console.log(error))
+
+    }, [id])
 
     return (
         <div>Journal Entry</div>
