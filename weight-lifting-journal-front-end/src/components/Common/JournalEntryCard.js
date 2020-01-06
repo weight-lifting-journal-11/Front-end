1e7d1708cea37d98d60bb8416bf48ce41ce8b251
CAM603 <cameronhawley@comcast.net>
2020-01-06 18:11:19 -0500

    added table in JournalEntryCard

--- weight-lifting-journal-front-end/src/components/Common/JournalEntryCard.js
+++ weight-lifting-journal-front-end/src/components/Common/JournalEntryCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 const JournalEntryCard = ({name, reps, sets, weight}) => {
-
+    
     return (
-        <div>
-            <h2>{name}</h2>
-            <h3>Reps: {reps}</h3>
-            <h3>Sets: {sets}</h3>
-            <h3>Weight: {weight}</h3>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Reps</th>
+                        <th>Sets</th>
+                        <th>Weight</th>
+                    </tr>
+                    <tr>
+                        <td>{name}</td>
+                        <td>{reps}</td>
+                        <td>{sets}</td>
+                        <td>{weight}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
