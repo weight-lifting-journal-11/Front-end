8f0d18c66800916aebcc9b9336ffe0068acf8d86
CAM603 <cameronhawley@comcast.net>
2020-01-09 15:34:35 -0500

    added more style

--- weight-lifting-journal-front-end/src/components/Styles/StyledLogin.js
+++ weight-lifting-journal-front-end/src/components/Styles/StyledLogin.js
@@ -11,16 +11,31 @@ export const StyledLogin = styled.div`
         justify-content: space-around;
         align-items: center;
         flex-direction: column;
-        height: 200px;
+        height: 250px;
         box-shadow: 10px 10px 5px grey;
         width: 40%;
         background: #FAFAFA;
         padding: 20px;
 
+        p {
+            color: #9A0A0A;
+            font-size: 1rem;
+            
+        }
+
         input {
             width: 50%;
             height: 30px;
             font-size: inherit;
+            margin: 10px 0;
+            border: solid #67E6A1 2px;
+            border-radius: 4px;
+            outline: none;
+
+            &:focus {
+                border: 2px solid #00A35E;
+            }
+
         }
         button {
             font-size: 1rem;
@@ -30,6 +45,7 @@ export const StyledLogin = styled.div`
             border: solid #00A35E 2px;
             background: #FFFFFF;
             color: #00A35E;
+            margin-top: 10px;
 
         &:hover {
             color: #FFFFFF;
